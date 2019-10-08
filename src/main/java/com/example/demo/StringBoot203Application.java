bdfea80402b0af8413cb3a687daa609e2ce79e1b
prasantib <prasanti.babu@gmail.com>
2019-10-08 10:08:55 -0400

    StringBoot_203a

---
+++ src/main/java/com/example/demo/StringBoot203Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot203Application.class, args);
+    }
+
+}
